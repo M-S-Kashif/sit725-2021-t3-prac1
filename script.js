094d58fdcf011c5e774328d80ba5590228c1384f
M-S-Kashif <94117269+M-S-Kashif@users.noreply.github.com>
2021-11-14 23:43:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//Adding two numericals...
+
+var a=2;
+var b=5;
+var sum = a+b;
+//console.log(sum);
+
+//Adding a number and a string...
+var b = "5";
+var humanSum = "The value of a: " + a + ".The value of b: "+ b + ".The final sum is: "+ sum +".";
+console.log(humanSum);
